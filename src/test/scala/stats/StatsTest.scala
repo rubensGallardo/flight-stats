1cec82699e2bcd32764dbb334b9483965672458d
rubensGallardo <rubengn13@gmail.com>
2022-09-28 23:13:48 +0200

    Tests

--- src/test/scala/stats/StatsTest.scala
+++ src/test/scala/stats/StatsTest.scala
@@ -1,15 +1,19 @@
-package stats
+package stats.test
 
-import org.scala.scalatest.{FunSuite}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import stats.test.SparkSessionTestWrapper
+import org.scalatest.FunSpec
 
-class StatsTest extends FunSuite:
-  val spark = SparkSession
-  .builder()
-  .appName("My first Spark App")
-  .config("spark.master", "local")
-  .getOrCreate()
+class StatsTest
+  extends FunSpec
+  with SparkSessionTestWrapper {
 
-  test("Stats.flightByMonth") {
-    assert true
+  it("test flights by month"){
+    val columns = Seq("passengerId","flightId","from","to","date")
+    val data = Seq (("48","0","cg","ir","2017-01-01"),("967","103","tk","co","2017-02-04"))
+    val flightsDf = spark.createDataFrame(data).toDF(columns:_*)
+    
+    
   }
+
+
+}
